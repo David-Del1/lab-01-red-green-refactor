126560d5d5d16a77bbe0fddf70e2d79c2085bbbd
David Delgadillo <daviddel.731@gmail.com>
2021-06-07 16:43:33 -0700

    first tests passing

--- redgreen.test.js
+++ redgreen.test.js
@@ -1,7 +1,22 @@
+import getName from './utils.js';
+
 describe('red/green refactor', () => {
 
-  it('returns name property of object', () => {
+  it('returns name spot', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    getName(spot);
 
+    expect(getName(spot)).toEqual('spot');
+  });
+
+  it('returns name Aang', () => {
+    const character = { 
+      _id: '5cf5679a915ecad153ab68c9', 
+      name: 'Aang' 
+    };
+    getName(character);
+
+    expect(getName(character)).toEqual('Aang');
   });
 
 
